2e8ca587c74b1a05b586678c533abe56a8c5d587
Bohdan Horbyk <bogdangorbik9@gmail.com>
2023-11-28 19:39:24 +0200

    Done

---
+++ src/apps/strings/Main.java
@@ -0,0 +1,65 @@
+package apps.strings;
+
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        // first task
+        System.out.println(wordsCount(""));
+        System.out.println(wordsCount("   "));
+        System.out.println(wordsCount(","));
+        System.out.println(wordsCount("Hello world"));
+        System.out.println(wordsCount("  Hello   world  "));
+        System.out.println(wordsCount("I love java!"));
+
+        // second task
+        System.out.println(isPalindrome(""));
+        System.out.println(isPalindrome("  "));
+        System.out.println(isPalindrome(","));
+        System.out.println(isPalindrome("ERE"));
+        System.out.println(isPalindrome("Allo"));
+        System.out.println(isPalindrome("Madam, I'm Adam"));
+        System.out.println(isPalindrome("  Madam, I'm Adam"));
+    }
+
+    /**
+     * Method counts the number of words in the String phrase.
+     * @param input String with phrase being checked
+     * @return the number of words in a phrase
+     */
+    private static int wordsCount(String input) {
+        // for works with more than one divider
+        input = input.trim().replaceAll("\\W+", " ");
+
+        if (input.isEmpty() || input.equals(" ")) {
+            return 0;
+        }
+
+        String[] words = input.split("\\s");
+        return words.length;
+    }
+
+    /**
+     * Method checks the input String phrase is a palindrome
+     * @param input String with phrase being checked
+     * @return true if so, otherwise - false.
+     */
+    private static boolean isPalindrome(String input) {
+       
+        input = input.trim().replaceAll("\\W+", " ");
+
+        if (input.isEmpty() || input.equals(" ")) {
+            return false;
+        }
+
+        String symbols = input.toLowerCase(Locale.ROOT).replaceAll("\\s", "");
+
+        for (int i = 0; i < symbols.length() / 2; i++) {
+            if (symbols.charAt(i) != symbols.charAt(symbols.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
